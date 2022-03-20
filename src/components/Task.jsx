4087e0fd63d55f7d109596cd1bf42c87af649e5e
zephyrus21 <pandey.piyus@gmail.com>
2022-03-20 20:48:42 +0530

    added tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import Modal from "./Modal";
 import styles from "../styles/Task.module.css";
 
 const Task = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getResult = async () => {
+      const result = await fetch("https://taskhit-api.herokuapp.com/tasks");
+      const data = await result.json();
+
+      setTasks(data);
+    };
+    getResult();
+  }, []);
 
   return (
     <div className={`${styles.task} flex`}>
       <div className='my-task'>
         <h3>Tasks</h3>
+        {tasks.map((task) => (
+          <div key={task._id}>
+            <p>{task.title}</p>
+            <p>{task.description}</p>
+            <p>{task.user}</p>
+          </div>
+        ))}
       </div>
       <div className='submitted-task'></div>
       <div className='add-task'>
