3890751a1ad450d5bb04f2d0d889e0f42f61a8ea
zephyrus21 <pandey.piyus@gmail.com>
2022-03-20 18:54:01 +0530

    added bug fix

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,15 +5,16 @@ import styles from "../styles/Task.module.css";
 
 const Task = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className={`${styles.task} flex`}>
-      <div className="my-task">
+      <div className='my-task'>
         <h3>Tasks</h3>
       </div>
-      <div className="submitted-task"></div>
-      <div className="add-task">
+      <div className='submitted-task'></div>
+      <div className='add-task'>
         <button onClick={() => setIsOpen(true)}>Add Task</button>
-        {isOpen && <Modal setOpen={setIsOpen} />}
+        {isOpen ? <Modal setIsOpen={setIsOpen} /> : <h1>Hello</h1>}
       </div>
     </div>
   );
